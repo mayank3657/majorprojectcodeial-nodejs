3dca680016053ffe057653c8db5fda0958088061
mayank3657 <mayank3657.be20@chutkara.edu.in>
2022-11-12 18:29:13 +0530

    First commit: create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const port=8000;
+const app=express();
+app.listen(port,function(err){
+    if(err){
+        // console.log('Error',err);
+        //or
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is running on port: ${port}`);
+});
