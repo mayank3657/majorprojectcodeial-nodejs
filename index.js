778312c70a8293d9718dc852eca6f2267690a80d
mayank3657 <mayank3657.be20@chutkara.edu.in>
2022-11-12 19:18:48 +0530

    add a controller and a router for home

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const port=8000;
 const app=express();
+app.use('/',require('./routes'));
 app.listen(port,function(err){
     if(err){
         // console.log('Error',err);
