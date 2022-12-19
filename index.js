1620aea1a0fcd4d61a8d1a8d8339ea128e6c0988
mayank3657 <mayank3657.be20@chutkara.edu.in>
2022-12-19 19:48:27 +0530

    codeial

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const port=8000;
 const app=express();
 app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         // console.log('Error',err);
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 module.exports=router;
